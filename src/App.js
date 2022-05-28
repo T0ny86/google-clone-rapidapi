11cfd3633a447db14d249290e276f4d1d3cd6add
Tanous Atallah <tanous.atallah@gmail.com>
2022-05-28 19:33:25 +0200

    App.js style is done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React, { useState } from 'react'
-import { Navbar } from './components/Navbar'
-import { Footer } from './components/Footer'
-import { Routes } from './components/Routes'
+import React, { useState } from 'react';
+
+import { Footer } from './components/Footer';
+import { Navbar } from './components/Navbar';
+import { Routes } from './components/Routes';
 
 const App = () => {
-  const [darkTheme, setDarkTheme] = useState(true)
+  const [darkTheme, setDarkTheme] = useState(false);
 
   return (
-    <div className={darkTheme ? 'dark' : ''} >
-      <div className='bg-gray-100'>
-        <Navbar />
+    <div className={darkTheme ? 'dark' : ''}>
+      <div className="dark:bg-gray-900 bg-gray-100 dark:text-gray-200 black min-h-screen">
+        <Navbar setDarkTheme={setDarkTheme} darkTheme={darkTheme} />
         <Routes />
         <Footer />
       </div>
-    </div >
-  )
-}
+    </div>
 
-export default App
+  );
+};
+
+export default App;
